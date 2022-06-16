48256355a6b6ee3756b812175d923200d61e12fd
Thalles Campagnani <thallescampagnani@gmail.com>
2022-06-16 01:59:15 -0300

    Criando variável para versão e alterando para rev10

--- CoppeliaSim-Linux-Install.sh
+++ CoppeliaSim-Linux-Install.sh
@@ -1,38 +1,45 @@
+#Variavel de versão
+CopSim=CoppeliaSim_Edu_V4_3_0_rev10_Ubuntu20_04
+
 clear
 echo "####################################################################################"
 echo "##										##"
-echo "##                          Instalando CoppeliaSim				##"
+echo "##                          Instalando CoppeliaSim EDU				##"
 echo "##										##"
 echo "####################################################################################"
 echo ""
 echo "---> Baixando instalador <---"
 echo ""
-wget -c https://www.coppeliarobotics.com/files/CoppeliaSim_Edu_V4_3_0_Ubuntu20_04.tar.xz
+wget -c https://www.coppeliarobotics.com/files/$CopSim.tar.xz
 echo ""
 echo "---> Instalando CoppeliaSim em /opt/ <---"
 echo ""
-sudo tar -xvf CoppeliaSim_Edu_V4_3_0_Ubuntu20_04.tar.xz -C /opt/
+sudo tar -xvf $CopSim.tar.xz -C /opt/
 echo ""
 echo "---> Deletando instalador <---"
 echo ""
-rm CoppeliaSim_Edu_V4_3_0_Ubuntu20_04.tar.xz
+rm $CopSim.tar.xz
+echo ""
+echo "---> Deletando instalação antiga (se houver) <---"
+echo ""
+rm /opt/CoppeliaSim
 echo ""
 echo "---> Renomeando instalação para /opt/CoppeliaSim <---"
 echo ""
-sudo mv /opt/CoppeliaSim_Edu_V4_3_0_Ubuntu20_04 /opt/CoppeliaSim
+sudo mv /opt/$CopSim /opt/CoppeliaSim
 echo ""
 echo "---> Baixando Icone <---"
 echo ""
 sudo wget -O /opt/CoppeliaSim/coppeliaSim.svg https://raw.githubusercontent.com/LabRobotica/CoppeliaSim-Linux-Install/main/coppeliaSim.svg
 echo ""
-echo "---> Criando Link simbólico coppeliaSim em /usr/bin <---"
+echo "---> Criando Link simbólico para CoppeliaSim em /usr/bin <---"
 echo ""
-sudo ln -s /opt/CoppeliaSim/coppeliaSim.sh /usr/bin/coppeliaSim
+sudo ln -sf /opt/CoppeliaSim/coppeliaSim.sh /usr/bin/coppeliaSim
 echo ""
 echo "---> Criando atalho no menu do sistema <---"
 echo ""
 sudo sh -c 'echo "[Desktop Entry]" > /usr/share/applications/coppeliaSim.desktop'
-sudo sh -c 'echo "Name=CoppeliaSim" >> /usr/share/applications/coppeliaSim.desktop'
+sudo sh -c 'echo "Name=CoppeliaSim EDU" >> /usr/share/applications/coppeliaSim.desktop'
 sudo sh -c 'echo "Comment=CoppeliaSim, anteriormente conhecido como V-REP, é um simulador robótico usado na indústria, educação e pesquisa." >> /usr/share/applications/coppeliaSim.desktop'
 sudo sh -c 'echo "Exec=coppeliaSim" >> /usr/share/applications/coppeliaSim.desktop'
 sudo sh -c 'echo "Icon=/opt/CoppeliaSim/coppeliaSim.svg" >> /usr/share/applications/coppeliaSim.desktop'
@@ -44,7 +51,7 @@ sudo sh -c 'echo "Keywords=V-REP;VREP;" >> /usr/share/applications/coppeliaSim.d
 clear
 echo "################################################################################"
 echo "###                                                                          ###"
-echo "###            Instalação do CoppeliaSim finalizada com sucesso              ###"
+echo "###            Instalação do CoppeliaSim EDU finalizada com sucesso          ###"
 echo "###                                                                          ###"
 echo "################################################################################"
 echo ""
